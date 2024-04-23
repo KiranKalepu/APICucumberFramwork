b2ccbb21adb24a80c72c54896d247f3af8bb7932
KiranKalepu <kiran.k@technovert.com>
2024-04-23 11:28:33 +0530

    completed the automation framework

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -9,10 +9,15 @@ public class Hooks {
 	@Before("@DeletePlace")
 	public void addPlace() throws IOException
 	{
+		if(ValidatePlaceStepDefination.placeId == null)
+		{
 		ValidatePlaceStepDefination sd = new ValidatePlaceStepDefination();
 		sd.add_place_api_payload_using_and("Kiran QA", "Selenium");
 		sd.user_hit_the_api_using_valid_data_with_http_request("AddPlace", "POST");
 		sd.get_place_api();
+		}
+		else 
+			return;
 	}
 
 }
